4d5302a6dbfd3a8257fdeadcf344fce1765927c6
Agung Adhinata <adhi0asta@gmail.com>
2024-11-18 17:04:42 +0800

    front: set backend as dependencies

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,27 @@
 "use client"
 
+import { useEffect, useState } from "react";
+
 
 export default function Page() {
-  function setupWeb3() {  
+  const [startup, setStartup] = useState(true)
+  async function setupWeb3() {  
     if(typeof window === "undefined") return;
     if(window.ethereum) {
+      await window.ethereum.request({ method: 'eth_requestAccounts' })
+      const id = await window.ethereum.request({method: "eth_chainId"})
       console.log("Ethereum enabled");
-      console.log(`Ethereum chainId: ${window.ethereum.chainId}`);
+      console.log(`Ethereum chainId: ${id}`);
     }
   }
-  setupWeb3();
+
+  useEffect(()=> {
+    if(startup) {
+      setStartup(false)
+      setupWeb3()
+    }
+  },[startup])
+
   return (
     <div>
       <h1>Page</h1>
---
+++ frontend/src/lib/etherContext.ts
@@ -0,0 +1,1 @@
+import CertitcationContract from "@blockchain-cert/backend/artifacts/contracts/Certification.sol/Certification.json"
