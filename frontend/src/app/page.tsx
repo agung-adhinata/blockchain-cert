2e7a82f124cc0e02a3b9273dca3102cf2ad70c71
Agung Adhinata <adhi0asta@gmail.com>
2024-11-18 17:32:50 +0800

    front: setup tailwind

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,30 +1,37 @@
-"use client"
+"use client";
 
+import { APP_TITLE } from "@/lib/constants";
 import { useEffect, useState } from "react";
 
-
 export default function Page() {
-  const [startup, setStartup] = useState(true)
-  async function setupWeb3() {  
-    if(typeof window === "undefined") return;
-    if(window.ethereum) {
-      await window.ethereum.request({ method: 'eth_requestAccounts' })
-      const id = await window.ethereum.request({method: "eth_chainId"})
-      console.log("Ethereum enabled");
-      console.log(`Ethereum chainId: ${id}`);
+  const [startup, setStartup] = useState(true);
+
+  async function setupWeb3() {
+    console.log("Setup Ethereum");
+    if (typeof window === "undefined") {
+      console.warn(`WIndow is missing, aborting...`);
+      return;
     }
+    if (!window.ethereum) {
+      console.warn(`Ether module is missing, aborting...`);
+      return;
+    }
+    console.log("Request chain ID");
+    const id = await window.ethereum.request({ method: "eth_chainId" });
+    console.log("Ethereum enabled");
+    console.log(`Ethereum chainId: ${id}`);
   }
 
-  useEffect(()=> {
-    if(startup) {
-      setStartup(false)
-      setupWeb3()
+  useEffect(() => {
+    if (startup) {
+      setupWeb3();
+      setStartup(false);
     }
-  },[startup])
+  }, [startup]);
 
   return (
     <div>
-      <h1>Page</h1>
+      <h1>{APP_TITLE}</h1>
     </div>
   );
 }
