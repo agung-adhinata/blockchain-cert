2f7150bc17441c6951337a6bd1258e8a472e2a5d
Agung Adhinata <adhi0asta@gmail.com>
2024-11-19 14:20:38 +0800

    front: move metamask login

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,8 @@ import { useEffect, useState } from "react";
 
 export default function Page() {
   const [startup, setStartup] = useState(true);
-
+  const [chainId, setChainId] = useState("");
+  const [accountId, setAccountId] = useState("");
   async function setupWeb3() {
     console.log("Setup Ethereum");
     if (typeof window === "undefined") {
@@ -16,21 +17,42 @@ export default function Page() {
       console.warn(`Ether module is missing, aborting...`);
       return;
     }
+    await window.ethereum.request({ method: "eth_requestAccounts" });
     console.log("Request chain ID");
     const id = await window.ethereum.request({ method: "eth_chainId" });
     console.log("Ethereum enabled");
     console.log(`Ethereum chainId: ${id}`);
+    const accountId = await window.ethereum.request({ method: "eth_accounts" });
+    if (Array.isArray(accountId)) setAccountId(accountId[0].toString());
+    if (typeof id === "string") {
+      setChainId(id);
+    }
   }
 
   useEffect(() => {
     if (startup) {
-      setupWeb3();
       setStartup(false);
     }
   }, [startup]);
 
   return (
     <div>
+      <header className="flex gap-2">
+        {chainId.length > 0 ? (
+          <>
+            <p>{chainId}</p>
+            <p>acc ID: {accountId}</p>
+          </>
+        ) : (
+          <button
+            onClick={() => {
+              setupWeb3();
+            }}
+          >
+            <p>Connect to metamask</p>
+          </button>
+        )}
+      </header>
       <h1>{APP_TITLE}</h1>
     </div>
   );
