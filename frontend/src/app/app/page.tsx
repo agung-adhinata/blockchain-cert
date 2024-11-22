f3ecafd4952c9030446801594014dbb34f00c34b
Agung Adhinata <adhi0asta@gmail.com>
2024-11-22 15:13:24 +0800

    front: update typesafety & contract

--- frontend/src/app/app/page.tsx
+++ frontend/src/app/app/page.tsx
@@ -1,37 +1,35 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { CertificationABI, CertificationAddress } from "@/lib/etherContext";
-import { ethers } from "ethers";
+import { BlockchainContext, ethersContext } from "@/layout/EthersContext";
 import Link from "next/link";
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 
 export default function AppPage() {
-  useEffect(()=> {
+  const blockchainContext = useContext(ethersContext) as BlockchainContext;
+  useEffect(() => {
     if (typeof window.ethereum !== "undefined") {
       try {
-        (async() => {
-          const provider = new ethers.BrowserProvider(window.ethereum!);
-          const signer = await provider.getSigner();
-          const contract = new ethers.Contract(CertificationAddress, CertificationABI.abi, provider);
-          const certificates = contract.getSignedCertificatesByOwner(signer.getAddress());
+        (async () => {
+          const certificates =
+            blockchainContext.contract?.getSignedCertificatesByOwner(
+              blockchainContext.signer?.getAddress()
+            );
           console.log(certificates);
-        })()
+        })();
       } catch (e) {
-      console.error(e);
+        console.error(e);
       }
-  }
-  },[])
-  
+    }
+  }, [blockchainContext.contract, blockchainContext.signer]);
+
   return (
     <div className="w-full h-full flex flex-col justify-center items-center bg-slate-300">
       <section className="p-4 flex flex-col gap-3 rounded min-w-96 bg-slate-200 shadow">
         <h1 className="text-3xl font-bold">E-Certificate</h1>
         <p>This is you list of newly created certificate</p>
         <Button asChild>
-          <Link href={"/app/create"}>
-          Create New Signed Certificate
-          </Link>
+          <Link href={"/app/create"}>Create New Signed Certificate</Link>
         </Button>
       </section>
     </div>
--- frontend/src/layout/EthersContext.tsx
+++ frontend/src/layout/EthersContext.tsx
@@ -1,30 +1,97 @@
 "use client";
+import { CertificationABI, CertificationAddress } from "@/lib/etherContext";
 import { BrowserProvider } from "ethers";
 import { ethers } from "ethers";
 import { createContext, useEffect, useState } from "react";
 
-const ethersContext = createContext<BrowserProvider | undefined>(undefined);
+// Certificate struct type
+interface Certificate {
+  id: string;
+  rootId: string;
+  prevId: string;
+  signedBy: string;
+  ipfsHash: string;
+  timestamp: bigint;
+  title: string;
+  description: string;
+}
+
+// use function from Certification.sol
+interface HardhatContractMethods {
+  // Contract methods
+  signCertificate(
+    _id: string,
+    _ipfsHash: string,
+    _title: string,
+    _description: string
+  ): Promise<void>;
+
+  editCertificate(
+    _rootId: string,
+    _newId: string,
+    _ipfsHash: string,
+    _title: string,
+    _description: string
+  ): Promise<void>;
+
+  getLatestCertificate(_rootId: string): Promise<Certificate>;
+
+  getCertificate(_certId: string): Promise<Certificate>;
+
+  getCertificates(_signedAddressOwner: string): Promise<Certificate[]>;
+
+  getCertificateHistory(_initialId: string): Promise<Certificate[]>;
+
+  // View mappings
+  certificatesById(id: string): Promise<Certificate>;
+  latestCertificateIdByRootId(rootId: string): Promise<string>;
+  certificateRootIdsBySignedAddress(address: string): Promise<string[]>;
+}
+
+type CertificationContract = ethers.Contract & HardhatContractMethods;
+
+export type BlockchainContext = {
+  provider: BrowserProvider;
+  signer: ethers.JsonRpcSigner | ethers.Signer;
+  contract: CertificationContract;
+};
+
+export const ethersContext = createContext<BlockchainContext | undefined>(
+  undefined
+);
 
 export function EthersProvider({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [web3, setWeb3] = useState<BrowserProvider | undefined>();
+  const [context, setContext] = useState<BlockchainContext | undefined>();
   const [startup, setStartup] = useState(true);
+
+  async function setupBlockchainContext(ethereum: ethers.Eip1193Provider) {
+    const provider = new ethers.BrowserProvider(ethereum);
+    const signer = await provider.getSigner();
+    const contract = new ethers.Contract(
+      CertificationAddress,
+      CertificationABI.abi,
+      provider
+    ) as CertificationContract;
+    setContext({ provider, signer, contract });
+  }
+
   useEffect(() => {
     if (startup) {
       setStartup(!startup);
       if (typeof window.ethereum !== "undefined") {
         try {
-          setWeb3(new ethers.BrowserProvider(window.ethereum));
+          setupBlockchainContext(window.ethereum);
         } catch (e) {
           console.error(e);
         }
       }
     }
-  }, [startup, web3]);
+  }, [startup, context]);
   return (
-    <ethersContext.Provider value={web3}>{children}</ethersContext.Provider>
+    <ethersContext.Provider value={context}>{children}</ethersContext.Provider>
   );
 }
