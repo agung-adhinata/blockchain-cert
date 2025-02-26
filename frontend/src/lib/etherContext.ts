35c5b4d01af1ef2a44a34738e167eeedb4405f8d
Agung Adhinata <adhi0asta@gmail.com>
2025-02-26 14:16:07 +0800

    feat: remove unused frontend

--- frontend/src/lib/etherContext.ts
+++
@@ -1,5 +0,0 @@
-import certification from "@blockchain-cert/backend/artifacts/contracts/Certification.sol/Certification.json"
-
-
-export const CertificationABI = certification
-export const CertificationAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
